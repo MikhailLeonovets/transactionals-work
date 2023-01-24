449ace33dd72fbebc3d446466339316e745fdd06
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2023-01-24 12:08:00 +0300

    transaction template added

--- src/main/java/com/techart/transactionals/controller/ProductController.java
+++ src/main/java/com/techart/transactionals/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.techart.transactionals.repository.entity.Product;
 import com.techart.transactionals.service.CounterService;
 import com.techart.transactionals.service.ProductService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,16 +20,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
     private final ProductService productService;
     private final CounterService counterService;
+    private final TransactionTemplate transactionTemplate;
 
-    public ProductController(final ProductService productService, final CounterService counterService) {
+    public ProductController(final ProductService productService,
+                             final CounterService counterService,
+                             final TransactionTemplate transactionTemplate) {
         this.productService = productService;
         this.counterService = counterService;
+        this.transactionTemplate = transactionTemplate;
     }
 
     @PostMapping("/save-update")
     public ResponseEntity<?> saveProductAndIncrementCounter(@RequestBody final ProductAndCounterDto productAndCounterDto) {
-        productService.save(new Product(productAndCounterDto.getProductName(), productAndCounterDto.getProductPrice()));
-        counterService.increment(productAndCounterDto.getCounterName());
+        transactionTemplate.execute(status -> {
+            productService.save(new Product(productAndCounterDto.getProductName(), productAndCounterDto.getProductPrice()));
+            counterService.increment(productAndCounterDto.getCounterName());
+            return "";
+        });
         return ResponseEntity.ok("Saved and Updated successfully");
     }
 }
